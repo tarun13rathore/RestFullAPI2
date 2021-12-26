9215f06189e32a9cf14d82d63df8dc67063fb610
tarun13rathore <tarun13rathore@gmail.com>
2021-12-26 12:13:10 +0530

    comment some line of code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const connectioDB = require("./DB/conn");
-const path = require("path");
+// const path = require("path");
 const app = express();
 const routes = require("./routes/index.js");
 const port = 3001;
 
 connectioDB();
-global.appRoot = path.resolve(__dirname);
+// global.appRoot = path.resolve(__dirname);
 app.use(express.json());
 app.use("/api", routes);
 
